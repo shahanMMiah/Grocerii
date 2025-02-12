1065e1ffa3dfda8c9af611f08a2db863e9f83934
shahanMMiah <shahan.m.miah@gmail.com>
2025-02-12 23:01:26 +0000

    fyne test

--- main.go
+++ main.go
@@ -1,72 +1,112 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	ings := MakeIngredients()
 
-	recipes := make([]recipe, 0)
+	a := app.New()
+	w := a.NewWindow("Hello")
 
-	rIngs := MakeIngredients()
-	rIngs = AddIngredients("cofee", 200.0, rune('c'), grams, rIngs)
+	w.Resize(fyne.NewSize(500, 500))
 
-	recipes = append(recipes, MakeRecipe("test", rIngs))
+	clock := widget.NewLabel("")
 
-	for {
-		fmt.Println("which command? -- add, view, save, read ")
+	go func() {
+		for t := range time.Tick(time.Second) {
 
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
+			h, m, s := t.Clock()
+			clock.SetText(fmt.Sprintf("time : %v : %v : %v", h, m, s))
+		}
+	}()
 
-		switch text {
-		case "add":
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		clock,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("MUSTARDDDDD")
+		}),
+	))
 
-			fmt.Println("name?")
+	w.ShowAndRun()
 
-			reader = bufio.NewReader(os.Stdin)
-			name, _ := reader.ReadString('\n')
-			name = strings.Replace(name, "\n", "", -1)
+	/*
 
-			fmt.Println("amount?")
+		import (
+		"bufio"
+		"fmt"
+		"os"
+		"strconv"
+		"strings"
+		)
 
-			reader = bufio.NewReader(os.Stdin)
-			amountStr, _ := reader.ReadString('\n')
-			amountStr = strings.Replace(amountStr, "\n", "", -1)
-			amount, _ := strconv.ParseFloat(amountStr, 64)
+		ings := MakeIngredients()
 
-			e := '♡'
-			ings = AddIngredients(name, amount, e, unt, ings)
+		recipes := make([]recipe, 0)
 
-		case "view":
+		rIngs := MakeIngredients()
+		rIngs = AddIngredients("cofee", 200.0, rune('c'), grams, rIngs)
 
-			fmt.Println("ingredient list:")
-			for _, i := range ings {
-				fmt.Printf("%v %c - amount: %v %v \n", i.Name, i.Emoji, i.Unit, i.Amount)
+		recipes = append(recipes, MakeRecipe("test", rIngs))
 
+		for {
+			fmt.Println("which command? -- add, view, save, read ")
+
+			reader := bufio.NewReader(os.Stdin)
+			text, _ := reader.ReadString('\n')
+			text = strings.Replace(text, "\n", "", -1)
+
+			switch text {
+			case "add":
+
+				fmt.Println("name?")
+
+				reader = bufio.NewReader(os.Stdin)
+				name, _ := reader.ReadString('\n')
+				name = strings.Replace(name, "\n", "", -1)
+
+				fmt.Println("amount?")
+
+				reader = bufio.NewReader(os.Stdin)
+				amountStr, _ := reader.ReadString('\n')
+				amountStr = strings.Replace(amountStr, "\n", "", -1)
+				amount, _ := strconv.ParseFloat(amountStr, 64)
+
+				e := '♡'
+				ings = AddIngredients(name, amount, e, unt, ings)
+
+			case "view":
+
+				fmt.Println("ingredient list:")
+				for _, i := range ings {
+					fmt.Printf("%v %c - amount: %v %v \n", i.Name, i.Emoji, i.Unit, i.Amount)
+
+				}
+
+			case "save":
+				WriteFile("test.json", ings, recipes)
+
+			case "read":
+				fmt.Println("file name?")
+				reader = bufio.NewReader(os.Stdin)
+				file, _ := reader.ReadString('\n')
+				file = strings.Replace(file, "\n", "", -1)
+
+				ings, recipes = ReadFile(file)
+				fmt.Printf("read in %v and %v", ings, recipes)
+
+			default:
+				return
 			}
 
-		case "save":
-			WriteFile("test.json", ings, recipes)
-
-		case "read":
-			fmt.Println("file name?")
-			reader = bufio.NewReader(os.Stdin)
-			file, _ := reader.ReadString('\n')
-			file = strings.Replace(file, "\n", "", -1)
-
-			ings, recipes = ReadFile(file)
-			fmt.Printf("read in %v and %v", ings, recipes)
-
-		default:
-			return
 		}
-
-	}
+	*/
 }
